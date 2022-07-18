16248373e039fa6b3378021d66a8ece5c34b6ac2
cal <caldayham@gmail.com>
2022-07-17 22:19:28 -0500

    added some basic header and a gitignore

---
+++ client-side/src/globalstyles.jsx
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+const PrimaryContainer = styled.div``;
+
+export { PrimaryContainer };
---
+++ client-side/src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { PrimaryContainer } from "../globalstyles";
+
+const HomePage = () => {
+  return (
+    <PrimaryContainer>
+      <h1>Cal Day Ham's HomePage</h1>
+    </PrimaryContainer>
+  );
+};
+
+export default HomePage;
